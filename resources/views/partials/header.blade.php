5311c3e7a4450f78fb66b41610c0a71b4e79048e
Arbaoui Mehdi <arbaoui.mehdi@gmail.com>
2018-07-19 01:08:54 +0100

    Add Global Styles

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,6 +1,15 @@
 <header class="banner">
   <div class="container">
-    <a class="brand" href="{{ home_url('/') }}">{{ get_bloginfo('name', 'display') }}</a>
+    <hgroup>
+      <h1 class="brand">
+        <a href="{{ home_url('/') }}">
+          {{ get_bloginfo('name', 'display') }}
+        </a>
+      </h1>
+      <h2>
+        {{ get_bloginfo('description', 'display') }}
+      </h2>
+    </hgroup>
     <nav class="nav-primary">
       @if (has_nav_menu('primary_navigation'))
         {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']) !!}
