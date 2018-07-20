e3af058ce13b9ae9a0e38b2f7ab833b31cac680e
Arbaoui Mehdi <arbaoui.mehdi@gmail.com>
2018-07-20 10:52:06 +0100

    Add Global Style

--- resources/views/partials/page-header.blade.php
+++ resources/views/partials/page-header.blade.php
@@ -1,3 +1,14 @@
 <div class="page-header">
   <h1>{!! App::title() !!}</h1>
+  @if (is_home())
+    <ul>
+      @foreach (get_categories() as $categorie)
+        <li>
+          <a href="{{ get_category_link($categorie->cat_ID) }}">
+              {{ $categorie->name }}
+          </a>
+        </li>
+      @endforeach
+    </ul>
+  @endif
 </div>
